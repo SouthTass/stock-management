277520c3b56f5301862026d2cd73e1a2e53fd685
wzt <wangzt@qll-times.com>
2022-03-25 14:14:10 +0800

    add

---
+++ src/views/Ainmation.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="home" class="home" :style="{'top': `${top}px`}">
+    <button class="button" @click="animationRender()">开始</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'animation',
+  data(){
+    return {
+      top: 0,
+      lastTime: 0,
+      elapsedTime: null
+    }
+  },
+  mounted() {
+    // this.animationRender();
+  },
+  methods: {
+    animationRender(){
+      let now = (+new Date())
+      if(this.lastTime !== 0) this.elapsedTime = (now - this.lastTime)
+      this.top += this.elapsedTime / 50
+      window.requestAnimationFrame(this.animationRender);
+      this.lastTime = now
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home{
+  width: 100%;
+  height: 2px;
+  background-color: red;
+  position: relative;
+}
+.button{
+  position: fixed;
+  top: 0;
+}
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    14314
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data(){
+    return {
+      
+    }
+  },
+  created(){
+    
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
